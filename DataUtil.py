17b21f163a0bcadb36afd27c2dc31bc340a489a4
bingnanp <pengbingnan@gmail.com>
2020-04-24 10:37:38 -0500

    add dataset files and change base uri

--- DataUtil.py
+++ DataUtil.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 class CleanUtil:
-    baseURI = 'D:/RiceClass/535/Indoor/UJIndoorLoc/'
+    baseURI = ''
     filename = ''
     df = None
 
